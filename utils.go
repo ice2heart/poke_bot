69b94e3b806e8e3d17ecfc902412fb07bfe438eb
Albert Iakupov <ice2heart@gmail.com>
2024-08-04 19:58:14 +0200

    Revert "updated firstN to improve readbility"
    This reverts commit 9efbc2a2d729629335047fd4ca8dc2bc007f2bce.

--- utils.go
+++ utils.go
@@ -6,11 +6,14 @@ import (
 )
 
 func firstN(s string, n int) string {
-	if len(s) <= n {
-		return s
-	} else {
-		return s[:n]
+	i := 0
+	for j := range s {
+		if i == n {
+			return s[:j]
+		}
+		i++
 	}
+	return s
 }
 
 type ParsedLink struct {
